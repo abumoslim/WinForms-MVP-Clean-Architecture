aa4d3510b41941782067c3e87668179923022e04
abumoslim <Al7addad515@google.com>
2026-05-23 16:10:09 +0300

    Update Interface IRepository

--- MyApp.Core/Interfaces/Repositories/IRepository.cs
+++ MyApp.Core/Interfaces/Repositories/IRepository.cs
@@ -7,7 +7,7 @@ namespace MyApp.Core.Interfaces.Repositories
     /// Defines write and read operations for a repository that manages entities.
     /// </summary>
     /// <typeparam name="T">The entity type managed by the repository.</typeparam>
-    public interface IRepository<T> where T : class
+    public interface IRepository<T> : IReadOnlyRepository<T> where T : class
     {
         /// <summary>
         /// Adds a new entity to the repository.
@@ -26,18 +26,5 @@ namespace MyApp.Core.Interfaces.Repositories
         /// </summary>
         /// <param name="entity">The entity to delete.</param>
         void Delete(T entity);
-
-        /// <summary>
-        /// Retrieves an entity by its identifier.
-        /// </summary>
-        /// <param name="id">The identifier of the entity.</param>
-        /// <returns>The matching entity, or <c>null</c> if none is found.</returns>
-        T GetById(int id);
-
-        /// <summary>
-        /// Retrieves all entities managed by the repository.
-        /// </summary>
-        /// <returns>An enumerable collection of entities.</returns>
-        IEnumerable<T> GetAll();
     }
 }
